bc4bc3423e25c58707d9c99aee9ef85c01f65792
Tin Chu <mchu.tinho@gmail.com>
2017-11-13 11:00:53 -0600

    First commit React Native

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a Component
+const App = () => (
+	<Text> Some Text</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
