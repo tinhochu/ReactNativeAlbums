12bea40850317055fb971ae49addbe20ed7a9599
Tin Chu <mchu.tinho@gmail.com>
2017-11-13 11:08:32 -0600

    header created

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import Libraries for making a components
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a Component
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+
+// Make the component available to other parts of the app
