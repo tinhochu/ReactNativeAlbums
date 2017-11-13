152ad6ed171430d7bf9f4320afc8bfcad39cbb3b
Tin Chu <mchu.tinho@gmail.com>
2017-11-13 11:52:43 -0600

    reusable Component

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make a Component
-const Header = () => {
+const Header = (props) => {
 	const { viewStyle, textStyle } = styles;
 
 	return (
 		<View style={viewStyle}>
-			<Text style={textStyle}>Albums!</Text>
+			<Text style={textStyle}>{props.headerText}</Text>
 		</View>
 	);
 };
