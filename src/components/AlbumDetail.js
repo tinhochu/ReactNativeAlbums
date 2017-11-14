ee1c85f2e77399d32164cd70318e5512d677b9e9
Tin Chu <mchu.tinho@gmail.com>
2017-11-14 10:23:44 -0600

    Button Functional Componnent

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, Image, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
 	const { title, artist, thumbnail_image, image } = album;
@@ -16,8 +17,8 @@ const AlbumDetail = ({ album }) => {
 	return (
 		<View>
 			<Card>
-				<CardSection>
 
+				<CardSection>
 					<View style={thumbnailContainerStyle}>
 						<Image
 							source={{ uri: thumbnail_image }}
@@ -29,12 +30,16 @@ const AlbumDetail = ({ album }) => {
 						<Text style={headerTextStyle}>{title}</Text>
 						<Text>{artist}</Text>
 					</View>
-
 				</CardSection>
 
 				<CardSection>
 					<Image style={imageStyle} source={{ uri: image }} />
 				</CardSection>
+
+				<CardSection>
+					<Button onPress={} buttonLabel={`buy ${title}`} />
+				</CardSection>
+
 			</Card>
 		</View>
 	);
