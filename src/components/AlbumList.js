daca039cf0fefa0868553d64c019eab591f5be45
Tin Chu <mchu.tinho@gmail.com>
2017-11-14 06:06:25 -0600

    album list ScrollView

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -24,9 +24,9 @@ class AlbumList extends Component {
 	render() {
 		console.log(this.state.albums);
 		return (
-			<View>
+			<ScrollView>
 				{ this.renderAlbums() }
-			</View>
+			</ScrollView>
 		);
 	}
 }
